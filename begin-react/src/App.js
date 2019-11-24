47ee5c3edaaf6233f7305af17992f09065e06101
kewon96 <yang0184@naver.com>
2019-11-25 02:32:02 +0900

    변수사용하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import HelloReact from './Hello';
 
 function App() {
+  const name = 'react'
   return (
     <>
       <HelloReact />
-      <HelloReact /> 
+      {/* 'name'이라는 문자열 그대로 출력됨 */}
+      <div>name</div> 
+      {/* 'react'라는 문자열값이 출력됨 */}
+      <div>{name}</div>
     </>
   );
 }
