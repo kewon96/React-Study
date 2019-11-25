a22b1e7b9d22d98691d61c268e8ef6f6a06791cf
Kewon <yang0184@naver.com>
2019-11-26 02:14:50 +0900

    ReactDOM.render 해석

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// App.js의 리턴을 id이름이 root인 곳에 넣어라 -> index.html의 root에 삽입됨
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
