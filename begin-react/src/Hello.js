8742a5361caf2bd93fc897cc4eb2285d691bb391
Kewon <yang0184@naver.com>
2019-11-26 02:55:56 +0900

    props

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-function HelloReact() {
-    return <div>Hello React!!!</div>
+function HelloReact({color, name}) {
+    return (
+        <div style={{color}}>
+            Hello React!!! {name}
+        </div>
+    )
+}
+
+HelloReact.defaultProps = {
+    name: '이름없음'
 }
 
 export default HelloReact
