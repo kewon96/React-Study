a1e47fac5438ce890f8185ca726fb3915c301b8c
Kewon <48837736+kewon96@users.noreply.github.com>
2020-03-22 20:01:30 +0900

    배열 요소 추가

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,9 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser'
 
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      // name값을 value로 덮어쓴다.
+      [name]: value
+    });
+  };
+
+  // Component 상태로서 관리
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'alpha',
@@ -19,20 +36,54 @@ function App() {
       username: 'gamma',
       email: 'gamma@naver.com'
     } 
-  ];
+  ]);
 
   // useState로 관리해도 무방함
   // 리랜더링되는 값이 아니기때문에 변수로 관리하는 것도 괜찮다는 것을 보여줌
-  const nextId = useRef(1);
+  const nextId = useRef(4);
 
   // 값이 바뀐다고해서 컴포넌트가 리랜더링이 될 이유가 없음
 
+  // 배열의 변화
+  // 기존의 배열에서 변화하는 것이 아닌 기존의 배열을 복사해서 구현하는 방식
+  // push, splice, sort는 복사해서 사용하지 않는 한 가급적 사용하면 안된다
+
   const onCreate = () => {
-    nextId.current += 1;
+    const user = {
+      id: nextId.current,
+      // username,
+      // email
+      ...inputs
+    };
+  
+  // 1. spread를 사용해 진행
+  // 여러개의 배열을 하나로 합칠 때 사용
+  setUsers([...users, user]);
+
+  // 2. concat을 사용해 진행
+  // 기존의 배열을 복사해서 해당 위치에 넣은 다음, 새로운 배열을 만든 뒤 추가
+  // Parameter에 배열이 아니어도 됨
+  setUsers(users.concat(user));
+  
+  // input 내용물 제거
+  setInputs({
+    username: '',
+    email: ''
+  });
+  
+  nextId.current += 1;
   }
 
   return (
-    <UserList users={users} />
+    <>
+      <CreateUser 
+        username={username} 
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}  
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
