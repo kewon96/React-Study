3b5aee09de7e0b3ab5cd2f2847687f820bcb8680
Kewon <48837736+kewon96@users.noreply.github.com>
2020-03-24 00:27:47 +0900

    useEffect

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
     const { id, username, email, active } = user;
+    // // mount / numount 
+    // useEffect(() => {
+    //     // 컴포넌트가 mount될 때 주로 추가하는 작업
+    //     // props로 받은 값을 컴포넌트의 state로 설정
+    //     // 외부 api요청
+    //     // D3, Video.js 등 외부 라이브러리 사용
+    //     // setInterval, setTimeout
+    //     console.log('컴포넌트가 화면에 나타남');
+
+    //     // 컴포넌트가 사라질 때 실행(unmount) === Cleaner
+    //     return () => {
+    //         // setInterval, setTimeout 사용해서 등록했던 작업을 제거할 때(clearInterval, clearTimeout)
+    //         // 라이브러리 인스턴스 제거
+    //         console.log('컴포넌트가 화면에서 사라짐');
+    //     }
+    // }, []); // 의존되는 값을 배열 안에 넣음, 값이 비어있으면 컴포넌트가 화면에 처음 나타날 때만 실행
+
+    // useEffect(() => {
+    //     console.log('user값이 설정됨!')
+    //     console.log(user);
+
+    //     return () => {
+    //         console.log('user값이 바뀌기 전!');
+    //         console.log(user)
+    //     }
+    // }, [user]); // 바뀔 때도 나타남
+    // // 배열 값을 넣어야 최신의 값을 얻을 수 있다.
+
+    // 해당 userEffect는 어떤 활동을 하건 모든 컴포넌트에서 실행된다.
+    // 이유는 부모컴포넌트가 리랜더링되면 자식컴포넌트 또한 리랜더링이 진행되는데
+    // User컴포넌트의 부모컴포넌트는 UserList인데 UserList에서 users배열이 바뀌게 된다면
+    // UserList가 리랜더링이 되고 자연스레 User컴포넌트도 리랜더링이 되기 때문이다.
+    // 브라우저상에는 원하는 항목만 이벤트가 작동하는 것을 볼 수 있지만
+    // DOM상에서는 모든 내용을 랜더링, 비교 후 바뀐 부분만 적용
+    // 지금은 느려지거나 하진 않지만 추후 데이터가 많아진다면 Virtual DOM에서의 리소스조차 아껴야하는 상황이 발생할 수 있다. => 컴포넌트 리랜더링 최적화
+    useEffect(() => {
+        console.log(user);
+    });
+
 
     return (
         <div>
